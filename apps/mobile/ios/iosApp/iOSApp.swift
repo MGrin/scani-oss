28a1d3fa94ac3b7e1f9fa262ad07f682b166385b
MGrin <mr6r1n@gmail.com>
2026-06-02 20:38:45 +0800

    feat(mobile): iOS Face ID gate + TabView shell wired to auth

--- apps/mobile/ios/iosApp/iOSApp.swift
+++ apps/mobile/ios/iosApp/iOSApp.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    @StateObject private var container = AppContainer()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(container: container)
         }
     }
 }
